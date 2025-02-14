e7bf63bdd118de2eba1fb43ed7de7218b1afd359
StufMuff <stuff_j@yahoo.com>
2025-02-13 19:12:59 -0600

    Added a config for path planner

---
+++ src/main/java/frc/robot/Util/RobotConfig.java
@@ -0,0 +1,23 @@
+package frc.robot.Util;
+
+public final class RobotConfig {
+
+    public static class RobotPropertiesConfig{
+        public double mass = 68;
+        public double moi = 15;
+    }
+
+    public static class BumberConfig{
+        public double bumpWidth = 0.2;
+        public double bumpLength = 1.5;
+        public double xOffset = 1;
+        public double yOffset = 1;
+    }
+
+    public static class ModuleConfig{
+        public double wheelRadius = 0.25;
+        public double driveGearing = 6.12;
+        public double maxDriveSpeed = 5;
+    }
+    
+}
