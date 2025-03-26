12985ae82242202417960263476f6fba3c59664f
JSON <salcedoparkertx@gmail.com>
2025-03-25 19:05:57 -0500

    added canrange

---
+++ src/main/java/frc/robot/Subsytems/RangeAligner/RangeAligner.java
@@ -0,0 +1,28 @@
+package frc.robot.Subsytems.RangeAligner;
+
+import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.core.CoreCANrange;
+
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RangeAligner extends SubsystemBase{
+    public CANrange leftCaNrange;
+    public CANrange rightCaNrange;
+
+    public RangeAligner(CANrange leftCaNrange, CANrange rightCaNrange) {
+        this.leftCaNrange = leftCaNrange;
+        this.rightCaNrange = rightCaNrange;
+    }
+
+    public Distance Range(Boolean isRight){
+        if(isRight){
+            return rightCaNrange.getDistance().getValue();
+        } else{
+            return leftCaNrange.getDistance().getValue();
+        }
+    }
+
+}
+
